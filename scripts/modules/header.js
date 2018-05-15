af68b6e289360b04303b2b0bb9ec2719707b52c6
Catherine Morgan <morgcat11@gmail.com>
2018-05-14 23:49:23 -0700

    tweaking

--- scripts/modules/header.js
+++ scripts/modules/header.js
@@ -17,7 +17,7 @@ export default class Header extends React.Component{
         </div>
         <div className="header-content">
           <h1>Hey, I'm Cat</h1>
-          <h2>I'm a software engineer.</h2>
+          <h2>I'm a software engineer</h2>
         </div>
       </section>
     );
