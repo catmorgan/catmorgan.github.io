c32bdaa31a5cb0bc6b037eecba607ef49b3e4869
Catherine Morgan <morgcat11@gmail.com>
2018-05-15 23:15:39 -0700

    More improvements

--- scripts/modules/articles.js
+++ scripts/modules/articles.js
@@ -8,13 +8,16 @@ export default class Articles extends React.Component{
       <section className="articles light-text">
         <div className="wrapper">
           <div className="article-content">
-            <a href="https://news.blizzard.com/en-us/blizzard/21752086/blizzard-interviews-cat-morgan" target="__blank">
+            <span>
               <img  src="./assets/images/article-preview.png"/>
               <div className="article-info">
                 <h1>Blizzard Interviews Cat Morgan</h1>
                 <p>"Don’t be afraid because you will find someone who is inspired by you and you will find people to support you."</p>
+                <a href="https://news.blizzard.com/en-us/blizzard/21752086/blizzard-interviews-cat-morgan" target="__blank" className="article-button">
+                  Check it out!
+                </a>
               </div>
-            </a>
+            </span>
           </div>
         </div>
       </section>
---
+++ scripts/modules/hobbies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Hobbies extends React.Component{
+
+  render(){
+    return (
+      <section className="hobbies light-text">
+        <div className="wrapper">
+          <h1><span>Me in a nut shell</span></h1>
+        </div>
+      </section>
+    );
+  }
+}
